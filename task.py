5ee6e0c17994c77edbafdf20fd16356c60782922
Gozeif <josephosama2005@outlook.com>
2026-02-20 23:21:51 +0200

    Add task class with status management and initialization

---
+++ task.py
@@ -0,0 +1,8 @@
+class task:
+    statuses = ["todo", "in-progress", "done"]
+    def __init__(self, id, description, status, createdAt, updatedAt):
+        self.id = id
+        self.description = description
+        self.status = status
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
