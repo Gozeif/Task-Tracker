e594a045344c3f518de9170c987eec45d5a354db
Gozeif <josephosama2005@outlook.com>
2026-02-21 00:17:18 +0200

    Initialize project structure with main program, task management, and JSON handling

--- src/task.py
+++ src/task.py
@@ -1,7 +1,7 @@
 import datetime
 
 
-class task:
+class Task:
     statuses = ["todo", "in-progress", "done"]
     def __init__(self, id, description, status, createdAt, updatedAt):
         self.id = id
@@ -15,3 +15,6 @@ class task:
             self.updatedAt = datetime.datetime.now()
         else:
             raise ValueError("Invalid status")
+    def update_description(self, new_description):
+        self.description = new_description
+        self.updatedAt = datetime.datetime.now()
